aaa00fb39ca592c76438b8b940954c03891122f1
Draco CHAU <draco.chau@cityu.edu.hk>
2021-03-31 14:02:36 +0800

    - fix pos col

--- main.py
+++ main.py
@@ -1,24 +1,50 @@
 # This is a sample Python script.
 import json
+import logging
+from abc import ABC, abstractmethod
 
 import click
 import pymysql
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s')
+
+
+class Exporter(ABC):
+    @abstractmethod
+    def insert(self, rec):
+        pass
+
+    @abstractmethod
+    def export(self):
+        return
+
+
+class JsonExporter(Exporter):
+    def __init__(self):
+        self.list = []
+
+    def insert(self, rec):
+        self.list.append(rec)
+
+    def export(self):
+        return json.dumps(self.list)
+
 
 @click.command()
 @click.option('--config', default="./config.json", help='Path of config file')
-@click.option('--args', default=[], help='Args in sql statement')
+@click.option('--args', help='Args in sql statement')
 def main(config, args):
     with open(config, "r") as f:
         config = json.load(f)
-
-        if len(args) > 0:
-            config['statement'] = config["sql"].format(*args)
+        if args is not None:
+            config['args'] = args.split(",")
         else:
-            config['statement'] = config["sql"]
-
+            config['args'] = []
+        exporter = JsonExporter()
         for rec in fetch_rows(**config):
-            print(rec)
+            exporter.insert(rec)
+        done = exporter.export()
+        print(done)
 
 
 def fetch_rows(**kwargs):
@@ -27,23 +53,21 @@ def fetch_rows(**kwargs):
                          user=kwargs['user'],
                          passwd=kwargs['password'],
                          db=kwargs['schema'],
-                         charset='utf8')
+                         charset='utf8',
+                         cursorclass=pymysql.cursors.DictCursor)
     try:
         cursor = db.cursor()
-        cursor.execute(kwargs['statement'])
+        logging.info('connected to database...')
+        logging.info('execute sql: {}'.format(kwargs['sql']))
+        logging.info('sql args: {}'.format(kwargs['args']))
+        num = cursor.execute(kwargs['sql'], kwargs['args'])
         results = cursor.fetchall()
-        col_def = {}
-
-        for i, c in enumerate(cursor.description):
-            col_def[c[0]] = i
-
+        logging.info('fetched {} rows'.format(num))
         for row in results:
-            record = {}
-            for i, n in enumerate(kwargs['columns']):
-                record[n] = row[col_def[n]]
-            yield record
+            yield row
     finally:
         db.close()
+        logging.info('database disconnected')
 
 
 # Press the green button in the gutter to run the script.
